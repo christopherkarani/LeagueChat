919c3bf04189c5662df558d4360da9ece8892c79
MAC USER <macuser@MACs-iMac.local>
2017-03-05 13:48:54 +0300

    Initial Commit

---
+++ LOLChat/RegisterView.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterVC.swift
+//  LOLChat
+//
+//  Created by MAC USER on 03/03/2017.
+//  Copyright © 2017 Neptune. All rights reserved.
+//
+
+import UIKit
+import ChameleonFramework
+
+class RegisterView : LoginRegisterViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = FlatPowderBlue()
+        tapToDismiss()
+        
+    }
+
+}
